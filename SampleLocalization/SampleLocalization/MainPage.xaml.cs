f11fab5a3999f0be3a0b7028179ca688cb9a87c6
matthieugd <mgd@virtew.com>
2010-10-17 23:01:31 -0400

    first checkin

---
+++ SampleLocalization/SampleLocalization/MainPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+using System.Threading;
+using SampleLocalization.Resources;
+
+namespace SampleLocalization
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void FrenchBt_Click(object sender, RoutedEventArgs e)
+        {
+            Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("fr-CA");
+            Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo("fr-CA");
+            ResetResources();
+        }
+
+        private void EnglishBt_Click(object sender, RoutedEventArgs e)
+        {
+            Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("en-US");
+            Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo("en-US");
+            ResetResources();
+        }
+
+        private void ResetResources()
+        {
+            ((LabelsManagerDynamic)App.Current.Resources["LabelsManagerDynamic"]).ResetResources();
+        }
+
+        
+    }
+}
---
+++ SampleLocalization/SampleLocalization/Resources/LabelsManagerDynamic.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.ComponentModel;
+using System.Linq.Expressions;
+
+namespace SampleLocalization.Resources
+{
+    public class LabelsManagerDynamic : INotifyPropertyChanged
+    {
+        private Labels labels;
+
+        public Labels Labels
+        {
+            get { return labels; }           
+        }
+
+        public LabelsManagerDynamic()
+        {
+            labels = new Labels();
+        }
+
+        public void ResetResources()
+        {
+            OnPropertyChanged(() => Labels);
+        }
+
+
+        #region INotifyPropertyChanged region
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged<T>(Expression<Func<T>> selector)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(GetPropertyNameFromExpression(selector)));
+            }
+        }
+
+        public static string GetPropertyNameFromExpression<T>(Expression<Func<T>> property)
+        {
+            var lambda = (LambdaExpression)property;
+            MemberExpression memberExpression;
+
+            if (lambda.Body is UnaryExpression)
+            {
+                var unaryExpression = (UnaryExpression)lambda.Body;
+                memberExpression = (MemberExpression)unaryExpression.Operand;
+            }
+            else
+            {
+                memberExpression = (MemberExpression)lambda.Body;
+            }
+
+            return memberExpression.Member.Name;
+        }
+        #endregion
+    }
+}
